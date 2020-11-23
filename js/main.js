3de3ddc15c905223b294d6edbf222769b9145896
LuisCitrone <70077190+LuisCitrone@users.noreply.github.com>
2020-11-23 23:00:30 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+    const modal = document.getElementById('id01');
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
